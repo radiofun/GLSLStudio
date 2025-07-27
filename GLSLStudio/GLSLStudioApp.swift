9226406c32fdc19a8d507a72e7918d305c74af79
Minsang Choi <radiofun8@gmail.com>
2025-07-26 21:00:21 -0700

    Initial Commit

---
+++ GLSLStudio/GLSLStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLSLStudioApp.swift
+//  GLSLStudio
+//
+//  Created by Minsang Choi on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GLSLStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
